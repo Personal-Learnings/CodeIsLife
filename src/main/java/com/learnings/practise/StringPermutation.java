203cdea21d1e0071eb0654a926d71ff2e33517a4
Madanraj Venkatesan <mjyashhome@Madanrajs-MacBook-Air.local>
2019-10-02 21:41:08 -0700

    Made StringPermutation class Better

--- src/main/java/com/learnings/practise/StringPermutation.java
+++ src/main/java/com/learnings/practise/StringPermutation.java
@@ -1,48 +1,34 @@
 package com.learnings.practise;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class StringPermutation {
 
     private boolean isStringPermutationOfOther_BigO_nSquare(String stringA, String stringB) {
         Set<String> permutations = new HashSet<>();
 
-        if(stringA.length() != stringB.length()) {
-            System.out.println("Length of " + stringA + " and " + stringB + " are not equal so it cannot be a permutation of the other.");
-            return false;
-        }
-        else if(stringA.equals(stringB)) {
-            System.out.println(stringA + " and " + stringB + " are equal so it is a permutation of the other.");
-            return true;
-        }
+        if(stringA.length() != stringB.length()) return false;
+        else if(stringA.equals(stringB)) return true;
+
         for(int i = 0; i < stringA.length(); i++) {
             Character pivot = stringA.charAt(i);
             StringBuilder remainingCharacters = new StringBuilder(stringA.substring(0, i) + stringA.substring(i + 1));
 
             for(int j = 0; j < stringA.length(); j++) {
-                if(j < remainingCharacters.length()) {
-                    permutations.add(remainingCharacters.insert(j, pivot).toString());
-                } else {
-                    permutations.add(remainingCharacters.append(stringA.charAt(i)).toString());
-                }
+                permutations.add(
+                        (j < remainingCharacters.length())
+                                ? remainingCharacters.insert(j, pivot).toString()
+                                : remainingCharacters.append(stringA.charAt(i)).toString()
+                );
                 remainingCharacters.deleteCharAt(j);
             }
         }
-        System.out.println(stringB + (permutations.contains(stringB) ? " is a " : " is not a ") + "permutation of " + permutations);
         return permutations.contains(stringB);
     }
 
     private boolean isStringPermutationOfOther_BigO_NLogN(String stringA, String stringB) {
-        if(stringA.length() != stringB.length()) {
-            System.out.println("Length of " + stringA + " and " + stringB + " are not equal so it cannot be a permutation of the other.");
-            return false;
-        }
-        else if(stringA.equals(stringB)) {
-            System.out.println(stringA + " and " + stringB + " are equal so it is a permutation of the other.");
-            return true;
-        }
+        if(stringA.length() != stringB.length()) return false;
+        else if(stringA.equals(stringB)) return true;
 
         char [] arrayA = stringA.toCharArray();
         char [] arrayB = stringB.toCharArray();
@@ -50,17 +36,10 @@ public class StringPermutation {
         Arrays.sort(arrayA);
         Arrays.sort(arrayB);
 
-        boolean isEqual = false;
         for(int i = 0; i < arrayA.length; i++) {
-            if(arrayA[i] != arrayB[i]) {
-                isEqual = false;
-                break;
-            }
-            isEqual = true;
+            if(arrayA[i] != arrayB[i]) return false;
         }
-        System.out.println(stringB + (isEqual ? " is a " : " is not a ") + "permutation of " + stringA);
-        return isEqual;
-
+        return true;
         //.equals and .toString is gonna take some time, so shifting to the above approach
         //boolean isEqual = Arrays.toString(arrayA).equals(Arrays.toString(arrayB));
         //System.out.println(stringB + (isEqual ? " is a " : " is not a ") + "permutation of " + stringA);
@@ -68,13 +47,7 @@ public class StringPermutation {
     }
 
     private boolean isStringPermutationOfOther_BigO_N(String stringA, String stringB) {
-        if (stringA.length() != stringB.length()) {
-            System.out.println("Length of " + stringA + " and " + stringB + " are not equal so it cannot be a permutation of the other.");
-            return false;
-        } else if (stringA.equals(stringB)) {
-            System.out.println(stringA + " and " + stringB + " are equal so it is a permutation of the other.");
-            return true;
-        }
+        if (stringA.length() != stringB.length()) return false;
 
         //Assuming the String is a Extended ASCII
         final int EXTENDED_ASCII_COUNT = 256;
@@ -82,52 +55,72 @@ public class StringPermutation {
         int [] countArray2 = new int[EXTENDED_ASCII_COUNT];
 
         for(int i = 0; i < stringA.length(); i++) {
+            if(stringA.charAt(i) > 256 || stringB.charAt(i) > 256) {
+                System.out.println("\nNot An Extended ASCII String");
+                return false;
+            }
             countArray1[stringA.charAt(i)]++;
             countArray2[stringB.charAt(i)]++;
         }
 
-        boolean isEqual = false;
         for(int j = 0; j < EXTENDED_ASCII_COUNT; j++) {
-            if(countArray1[j] != countArray2[j]) {
-                isEqual = false;
-                break;
-            }
-            isEqual = true;
+            if(countArray1[j] != countArray2[j]) return false;
         }
-        System.out.println(stringB + (isEqual ? " is a " : " is not a ") + "permutation of " + stringA);
-        return isEqual;
+        return true;
+    }
+
+    private boolean isStringPermutationOfOther_BigO_N_lesserSpace(String stringA, String stringB) {
+        if (stringA.length() != stringB.length()) return false;
+
+        //Assuming the String is a Extended ASCII
+        final int EXTENDED_ASCII_COUNT = 256;
+        int [] countArray = new int[EXTENDED_ASCII_COUNT];
+
+        for(int i = 0; i < stringA.length(); i++) {
+            if(stringA.charAt(i) > 256 || stringB.charAt(i) > 256) {
+                System.out.println("\nNot An Extended ASCII String");
+                return false;
+            }
+            countArray[stringA.charAt(i)]++;
+        }
+
+        for(int j = 0; j < stringB.length(); j++) {
+            int index = stringB.charAt(j);
+            countArray[index]--;
+            if(countArray[index] < 0) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
+
         StringPermutation stringPermutation = new StringPermutation();
-        stringPermutation.isStringPermutationOfOther_BigO_nSquare("AMC", "CAM");
-        stringPermutation.isStringPermutationOfOther_BigO_nSquare("SMC", "MCS");
-        stringPermutation.isStringPermutationOfOther_BigO_nSquare("SSC", "SCS");
-        stringPermutation.isStringPermutationOfOther_BigO_nSquare("AAA", "BBA");
-        stringPermutation.isStringPermutationOfOther_BigO_nSquare("$$", "BBA");
-        stringPermutation.isStringPermutationOfOther_BigO_nSquare("﷽﷽", "\uD809\uDC29");
-        stringPermutation.isStringPermutationOfOther_BigO_nSquare("\uD809\uDC29", "\uD809\uDC29");
+        Map<String, String> inputMap = new LinkedHashMap<>();
+        inputMap.put("AMC", "CAM");
+        inputMap.put("SMC", "MCS");
+        inputMap.put("SSC", "SCS");
+        inputMap.put("AAA", "BBA");
+        inputMap.put("$$", "BBA");
+        inputMap.put("﷽﷽", "\uD809\uDC29");
+        inputMap.put("\uD809\uDC29", "\uD809\uDC29");
 
+        inputMap.forEach((stringA, stringB) -> {
+            System.out.println(stringA + " & " + stringB + " => " + stringPermutation.isStringPermutationOfOther_BigO_nSquare(stringA, stringB));
+        });
         System.out.println("-------------------------------------");
 
-        stringPermutation.isStringPermutationOfOther_BigO_NLogN("AMC", "CAM");
-        stringPermutation.isStringPermutationOfOther_BigO_NLogN("SMC", "MCS");
-        stringPermutation.isStringPermutationOfOther_BigO_NLogN("SSC", "SCS");
-        stringPermutation.isStringPermutationOfOther_BigO_NLogN("AAA", "BBA");
-        stringPermutation.isStringPermutationOfOther_BigO_NLogN("$$", "BBA");
-        stringPermutation.isStringPermutationOfOther_BigO_NLogN("﷽﷽", "\uD809\uDC29");
-        stringPermutation.isStringPermutationOfOther_BigO_NLogN("\uD809\uDC29", "\uD809\uDC29");
-
+        inputMap.forEach((stringA, stringB) -> {
+            System.out.println(stringA + " & " + stringB + " => " + stringPermutation.isStringPermutationOfOther_BigO_NLogN(stringA, stringB));
+        });
         System.out.println("-------------------------------------");
 
-        stringPermutation.isStringPermutationOfOther_BigO_N("AMC", "CAM");
-        stringPermutation.isStringPermutationOfOther_BigO_N("SMC", "MCS");
-        stringPermutation.isStringPermutationOfOther_BigO_N("SSC", "SCS");
-        stringPermutation.isStringPermutationOfOther_BigO_N("AAA", "BBA");
-        stringPermutation.isStringPermutationOfOther_BigO_N("$$", "BBA");
-        stringPermutation.isStringPermutationOfOther_BigO_N("AAA", "aaa");
+        inputMap.forEach((stringA, stringB) -> {
+            System.out.println(stringA + " & " + stringB + " => " + stringPermutation.isStringPermutationOfOther_BigO_N(stringA, stringB));
+        });
+        System.out.println("-------------------------------------");
 
-        //stringPermutation.isStringPermutationOfOther_BigO_N("﷽﷽", "\uD809\uDC29");
-        //stringPermutation.isStringPermutationOfOther_BigO_N("\uD809\uDC29", "\uD809\uDC29");
+        inputMap.forEach((stringA, stringB) -> {
+            System.out.println(stringA + " & " + stringB + " => " + stringPermutation.isStringPermutationOfOther_BigO_N_lesserSpace(stringA, stringB));
+        });
     }
 }
