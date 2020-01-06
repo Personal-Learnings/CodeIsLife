2a740dc18ef329effb2e6d8af66abc4508be3a2e
Madanraj Venkatesan <mjyashhome@Madanrajs-MacBook-Air.local>
2020-01-05 20:18:53 -0800

    Solved Few More leet Code Problems

--- src/main/java/com/learnings/practise/problems/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/learnings/practise/problems/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,6 +1,31 @@
 package com.learnings.practise.problems.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringWithoutRepeatingCharacters {
+
+    //Time Complexity: O(2N) => O(N) //Sliding Window Pattern //Assuming the string is ASCII //Using Array //Best Running Time
+    public int lengthOfLongestSubstring_3(String s) {
+        if(s == null || s.length() == 0) return 0;
+
+        int [] cache = new int[128];
+        int result = Integer.MIN_VALUE;
+
+        char[] charArray = s.toCharArray();
+        for(int windowEnd = 0, windowStart = 0; windowEnd < charArray.length; windowEnd++) {
+            char currentChar = charArray[windowEnd];
+            cache[currentChar]++;
+
+            while(cache[currentChar] > 1) {
+                char startWindowChar = charArray[windowStart];
+                cache[startWindowChar]--;
+                windowStart++;
+            }
+            result = Math.max(result, windowEnd - windowStart + 1);
+        }
+        return result;
+    }
 
     //Time Complexity: O(N^2)
     public int lengthOfLongestSubstring(String s) {
@@ -20,7 +45,29 @@ public class LongestSubstringWithoutRepeatingCharacters {
         return length;
     }
 
-    //Time Complexity: O(N)
+    //Time Complexity: O(2N) => O(N) //Sliding Window Pattern
+    public int lengthOfLongestSubstring_2(String s) {
+        if(s == null || s.length() == 0) return 0;
+
+        Map<Character, Integer> map = new HashMap<>();
+        int result = Integer.MIN_VALUE;
+
+        char[] charArray = s.toCharArray();
+        for(int windowEnd = 0, windowStart = 0; windowEnd < charArray.length; windowEnd++) {
+            char currentChar = charArray[windowEnd];
+            map.put(currentChar, map.getOrDefault(currentChar, 0) + 1);
+
+            while(map.get(currentChar) > 1) {
+                char startWindowChar = charArray[windowStart];
+                map.put(startWindowChar, map.get(startWindowChar) - 1);
+                windowStart++;
+            }
+            result = Math.max(result, windowEnd - windowStart + 1);
+        }
+        return result;
+    }
+
+    //Time Complexity: O(N) // From Solution
     public int lengthOfLongestSubstring_1(String s) {
         int result = 0;
         int[] cache = new int[256];
@@ -34,6 +81,13 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
     public static void main(String[] args) {
         System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("abcabcbb"));
-        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring_1("abcabcbb"));
+
+        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring_2("abcabcbb"));
+        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring_2("pwwkew"));
+        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring_2("tmmzuxt"));
+
+        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring_3("abcabcbb"));
+        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring_3("pwwkew"));
+        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring_3("tmmzuxt"));
     }
 }
