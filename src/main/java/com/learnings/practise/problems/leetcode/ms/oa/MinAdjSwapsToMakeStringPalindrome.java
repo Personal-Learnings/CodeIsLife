ef68419f9c45bc1a145e4b90e80124bd66e2a7ee
Madanraj Venkatesan <mjyashhome@Madanrajs-MacBook-Air.local>
2020-02-27 22:15:19 -0800

    Solved Few More leet Code Problems

--- src/main/java/com/learnings/practise/problems/leetcode/ms/oa/MinAdjSwapsToMakeStringPalindrome.java
+++ src/main/java/com/learnings/practise/problems/leetcode/ms/oa/MinAdjSwapsToMakeStringPalindrome.java
@@ -6,6 +6,20 @@ public class MinAdjSwapsToMakeStringPalindrome {
 
     private List<String> combinationsForPrint;
 
+    /**
+     * Algorithm:
+     *     1. First check the given string is a jumbled/shuffled palindrome or not.
+     *     2. Next have two pointers p1 at 0 and p2 at s.length - 1 and start iterating.
+     *     3. If chars at both the pointers are same then just shrink the window (increase the p1 and decrease the p2).
+     *     4. or Else
+     *          a. find the matching pair and swap the char to p2 index (increase swap count while swapping) and finally shrink the window.
+     *          b. if not found just swap once with adjacent index and increase the swap count (do not shrink the window here)
+     *     5. Print the Swap Count
+     *
+     * Time Complexity: O(n) to find Palindrome + [ O(n) for two pointer iteration * O(n) for checking and swapping ] so => O(n^2)
+     * Space Complexity: O(n)
+     *
+     */
     private int getNoOfSwaps(String s) {
         combinationsForPrint = new ArrayList<>();
 
